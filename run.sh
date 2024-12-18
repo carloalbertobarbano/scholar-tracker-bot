daaa8effe3f78aecb6691a6e537b21f2a0c82691
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2024-12-18 23:36:28 +0100

    add -d

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 DATA_DIR=$(pwd)/data
 CONFIG_FILE=$(pwd)/conf.json
 
-docker run --rm -v ${DATA_DIR}:/app/data -v ${CONFIG_FILE}:/app/conf.json ghcr.io/carloalbertobarbano/scholar-tracker-bot:main
+docker run --rm -d -v ${DATA_DIR}:/app/data -v ${CONFIG_FILE}:/app/conf.json ghcr.io/carloalbertobarbano/scholar-tracker-bot:main
